f63ad5d0cfd91669d0ed7e6a4b0294f94f926353
hormonic <hormonic@github.com>
2024-09-19 05:17:29 +0000

    240919 14:17 jm

---
+++ myapp/main.py
@@ -0,0 +1,11 @@
+# /home/kjm/work/ch11/ex01/github-actions-practice/myapp/main.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world();
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8001)
